5a9b24d1d3f481519873d5607d8745d410d45202
Alexandre Savio <alexsavio@gmail.com>
2014-10-29 15:51:17 +0100

    Add instance.py skeleton

---
+++ darwin/instance.py
@@ -0,0 +1,31 @@
+"""
+YAML Class Instantiator
+"""
+import yaml
+
+
+def import_module(module_path):
+    """Import any module to the global Python environment
+
+    Parameters
+    ----------
+    module_path: str
+        Path to the module to be imported
+
+
+
+    """
+
+class Instantiator():
+    """
+    YAML Class Instantiator for classifiers and feature selections methods.
+    For now, it only works on classes with the scikit-learn interface.
+
+    Parameters
+    ----------
+    """
+
+    def __init__(self, ymlpath):
+
+
+    def get_instance(self, class_name):
