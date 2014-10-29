963162e8df1574ee34165af670f87ace3b8b0542
Alexandre Savio <alexsavio@gmail.com>
2014-10-29 17:26:13 +0100

    Improve import_class function

--- darwin/instance.py
+++ darwin/instance.py
@@ -3,6 +3,36 @@ YAML Class Instantiator
 """
 import sys
 import yaml
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def import_class(class_module_path):
+    """Import any class to the global Python environment.
+       The module_path argument specifies what class to import in
+       absolute or relative terms (e.g. either pkg.mod or ..mod).
+       If the name is specified in relative terms, then the package argument
+       must be set to the name of the package which is to act as the anchor
+       for resolving the package name.
+
+    Parameters
+    ----------
+    module_path: str
+        Path to the module to be imported
+
+    Returns
+    -------
+    The specified module will be inserted into sys.modules and returned.
+    """
+    import importlib
+    try:
+        mod = importlib.import_module(class_module_path)
+        class_name = class_module_path.split('.')[-1]
+        return getattr(mod, class_name)
+    except:
+        log.exception('Error importing class {}.'.format(class_module_path))
+        raise
 
 
 def import_module(module_path):
@@ -24,8 +54,12 @@ def import_module(module_path):
     The specified module will be inserted into sys.modules and returned.
     """
     import importlib
-    mod = importlib.import_module(module_path)
-    return mod
+    try:
+        mod = importlib.import_module(module_path)
+        return mod
+    except:
+        log.exception('Error importing module {}.'.format(module_path))
+        raise
 
 
 def import_pyfile(filepath, mod_name=None):
