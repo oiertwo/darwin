f82213e9d63e4602ff75e8ccfdb3dad8eee3e3f5
Alexandre Savio <alexsavio@gmail.com>
2014-10-29 16:05:38 +0100

    Add import_module function

--- darwin/instance.py
+++ darwin/instance.py
@@ -1,20 +1,55 @@
 """
 YAML Class Instantiator
 """
+import sys
 import yaml
 
 
 def import_module(module_path):
-    """Import any module to the global Python environment
+    """Import any module to the global Python environment.
+       The module_path argument specifies what module to import in
+       absolute or relative terms (e.g. either pkg.mod or ..mod).
+       If the name is specified in relative terms, then the package argument
+       must be set to the name of the package which is to act as the anchor
+       for resolving the package name (e.g. import_module('..mod', 'pkg.subpkg')
+       will import pkg.mod).
 
     Parameters
     ----------
     module_path: str
         Path to the module to be imported
 
-
-
+    Returns
+    -------
+    The specified module will be inserted into sys.modules and returned.
     """
+    import importlib
+    mod = importlib.import_module(module_path)
+    return mod
+
+
+def import_pyfile(filepath, mod_name=None):
+    """
+    Imports the contents of filepath as a Python module.
+
+    :param filepath: string
+
+    :param mod_name: string
+    Name of the module when imported
+
+    :return: module
+    Imported module
+    """
+    if sys.version_info.major == 3:
+        import importlib.machinery
+        loader = importlib.machinery.SourceFileLoader('', filepath)
+        mod = loader.load_module(mod_name)
+    else:
+        import imp
+        mod = imp.load_source(mod_name, filepath)
+
+    return mod
+
 
 class Instantiator():
     """
