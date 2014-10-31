86061c7ba29023d3e65a930234ccfb94b2f4a942
Alexandre Savio <alexsavio@gmail.com>
2014-10-31 16:16:13 +0100

    Fix logger config

--- darwin/logger.py
+++ darwin/logger.py
@@ -6,14 +6,13 @@ import logging.config
 from .config import LOG_LEVEL
 
 
-def setup_logging(default_path=os.path.join(os.path.dirname(__file__),
-                                            'logger.yml'),
-                  default_level=LOG_LEVEL,
+def setup_logging(log_config_file=os.path.join(os.path.dirname(__file__), 'logger.yml'),
+                  log_default_level=LOG_LEVEL,
                   env_key='DARWIN_LOG_CFG'):
     """Setup logging configuration
 
     """
-    path = default_path
+    path = log_config_file
     value = os.getenv(env_key, None)
     if value:
         path = value
@@ -23,7 +22,7 @@ def setup_logging(default_path=os.path.join(os.path.dirname(__file__),
         logging.config.dictConfig(config)
         #print('Started logging using config file {0}.'.format(path))
     else:
-        logging.basicConfig(level=default_level)
+        logging.basicConfig(level=log_default_level)
         #print('Started default logging. Could not find config file '
         #      'in {0}.'.format(path))
     log = logging.getLogger(__name__)
